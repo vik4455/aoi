5105286bfa364335d91fb778db345c75dd9c4630
Vikrom Sompong <vikrom@Vikroms-MacBook-Pro.local>
2017-11-29 01:26:35 +0700

    AOI Test

--- bot.php
+++ bot.php
@@ -23,30 +23,8 @@ if (!is_null($events['events'])) {
 		
         if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
             
-            $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-
-            $server = $url["host"];
-            $username = $url["user"];
-            $password = $url["pass"];
-            $db = substr($url["path"], 1);
-
-            $conn = new mysqli($server, $username, $password, $db);
+            include 'include/connect.php';
             
-            if (mysqli_connect_errno()) {
-                printf("Connect failed: %s\n", mysqli_connect_error());
-            exit();
-            }
-
-            /* return name of current default database */
-            if ($result = $conn->query("SELECT DATABASE()")) {
-            $row = $result->fetch_row();
-            $result->close();
-            }
-
-/* change db to world db */
-            $conn->select_db("heroku_88c65b266d41d8b");
-
-            $conn->set_charset("utf8");
             $inc = $conn->query('SELECT * FROM phone');
             $inc_c = $inc->num_rows;
             
