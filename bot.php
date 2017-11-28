61d5c14e1db3bb163e73fd70ce637acc6e466013
Vikrom Sompong <vikrom@Vikroms-MacBook-Pro.local>
2017-11-29 02:35:08 +0700

    AOI Test

--- bot.php
+++ bot.php
@@ -47,9 +47,7 @@ if (!is_null($events['events'])) {
                             $respMessage='เบอร์ของ '.$txttel[1].' คือ '.$tt['phone'];
                             break; 
                     default:
-                            $respMessage='พิมพ์           
-                            m,ชื่อเพื่อน,เบอร์โทร เพื่อบันทึก  
-                            s,ชื่อเพื่อน เพื่อค้นหา'; 
+                            $respMessage='พิมพ์     - m,ชื่อเพื่อน,เบอร์โทร เพื่อบันทึก     -s,ชื่อเพื่อน เพื่อค้นหา'; 
                             break;
                 }
             } 
